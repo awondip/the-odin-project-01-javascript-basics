2311420db57a526e104dda3708ecbfa34f65a24d
dylane <awondipdylane047@.com>
2022-03-04 12:45:35 +0100

    basic understanding of const let var

--- declearingVariables.js
+++ declearingVariables.js
@@ -1,2 +1,29 @@
 // writing comment in javascript
-//this files deals with all way of declearing variables
+//this files deals with all way of declearing variable
+
+//first way to declear variable
+const boy = "adam";
+
+// console.log(boy); 
+
+// boy = "boris";
+
+// console.log(boy)
+
+
+//second to declear variable
+let girl = "angel";
+
+console.log(girl);
+
+ girl = "eva";
+console.log(girl)
+
+// third to declear variable
+
+var car = "toyota";
+console.log(car)
+car = "mercedes"
+console.log(car)
+
+// date:04/03/2022 diffrences between var and let
