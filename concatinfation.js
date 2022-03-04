f84e2e70f83476ce75c879aaf2d00d40ba0a6867
dylane <awondipdylane047@.com>
2022-03-04 13:17:46 +0100

    operators

---
+++ concatinfation.js
@@ -0,0 +1,12 @@
+let str1 = "dylane";
+let str2 = "mohamad";
+
+console.log(str1, str2)
+
+//making a sentense
+let sentense = str2 + " is tutoring "+ str1;
+let sentense2 = str2.concat(" is tutoring again ").concat(str1);
+let sentense3 = str1 + " is number "+1 +" student"
+
+console.log(typeof(sentense3))
+
---
+++ operators.js
@@ -0,0 +1,10 @@
+let add = 1+1;
+let multiply = 2*2;
+let division = 6/3;
+let square = 4**2;
+let remeder = 4%2;
+let num = 1;
+num++
+let incerement = num
+
+console.log("add",add, "multiply ",multiply, "division ",division, "squre ", square, "remender ",remeder, "increment", incerement)
