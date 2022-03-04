04f48470ea8418f9cc621f46df2d2e86b740af89
dylane <awondipdylane047@.com>
2022-03-04 15:38:08 +0100

    Explaning NaN result

---
+++ NaN-result.js
@@ -0,0 +1,15 @@
+//NaN stands for Not a Number and It represents a value which is not a valid number.
+//It can be used to check whether a number entered is a valid number or not a number.
+//example
+let a = 10;
+let b = "mango";
+console.log(a / b)
+
+let x = NaN;
+let y = 5;
+console.log(x + y)
+
+let c = NaN;
+let d = "5";
+let z = x + y;
+console.log(x + y)
