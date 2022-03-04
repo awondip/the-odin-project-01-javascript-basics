38f4573136f690ee071080d61d51f0f4abca7bc6
dylane <awondipdylane047@.com>
2022-03-04 14:47:58 +0100

    tribble equals

---
+++ tribble-equals.js
@@ -0,0 +1,12 @@
+//=== (Triple equals) is a strict equality comparison operator in JavaScript, which returns false for the values which are not of a similar type.
+// example
+var e = "hello"
+var f = "hello"
+console.log( e === f); 
+
+ var e = "hello"
+ var f = "hola"
+console.log( e === f);    
+
+console.log(3 === 3);               
+console.log(3 === 4);
